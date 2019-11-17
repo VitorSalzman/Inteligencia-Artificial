3e3e7925a251477f42fe0a9aa5578ca52642d3b1
Luiz Antonio R. G <luizguzzo@gmail.com>
2019-11-17 16:39:31 -0300

    terminado ag_new
    salz, essa teve o melhore resultado no de 20 gerações

--- algoritmo_genetico/ag/ag_real.py
+++ algoritmo_genetico/ag/ag_real.py
@@ -8,7 +8,7 @@ class Cromossomo():
 
     def __init__(self):
         self.gene = 0
-        self.aptidao = None
+        self.aptidao = 0
 
     def gera_gene(self):
         self.gene = random.uniform(-20,20)
@@ -43,7 +43,9 @@ class Cromossomo():
         self.aptidao = apt
         return self.aptidao
 
-
+    def setGene(self,gene):
+        self.gene = gene
+        self.calc_aptidao()
     
     def getGene(self):
         return self.gene
@@ -58,11 +60,11 @@ def mutation(vetCromo,g,gmax):
         cromo.mutation(g,gmax)
     return vetCromo
 
-def calc_aptidao(vetCromo):
-    for cromo in vetCromo:
-        b = cromo.calc_aptidao()
-       # print("apt: {} | norm {}".format(b))
-    return vetCromo
+# def calc_aptidao(vetCromo):
+#     for cromo in vetCromo:
+#         b = cromo.calc_aptidao()
+#        # print("apt: {} | norm {}".format(b))
+#     return vetCromo
 
 def torneio(cromossomos):
     novovetor_cromossomos = [Cromossomo() for _ in range(10)]
@@ -76,7 +78,7 @@ def torneio(cromossomos):
             novovetor_cromossomos[i] = (cromossomos[r2])
         else:
             novovetor_cromossomos[i] = (cromossomos[r1])
-    # print(novovetor_cromossomos[i].genes)
+        print(novovetor_cromossomos[i].getAptidao())
     return novovetor_cromossomos
 
 def crossOver(cromossomos):
@@ -87,24 +89,35 @@ def crossOver(cromossomos):
         # poderiamos verificar se é o mesmo numero
         chance = randint(0,100)
         if chance <= globalcross:
-
+            print("CrossOvered - [{},{}]".format(r1,r2))
             B = random.uniform(-A, 1 + A)
 
-            cromo1 = cromossomos[r1].getGene()
-            cromo2 = cromossomos[r2].getGene()
+            cromo1 = cromossomos[r1]
+            cromo2 = cromossomos[r2]
 
-            print("Antes")
-            print("p1:{}|p2:{}".format(cromo1, cromo2))
+            print("Antes - p1:{}|p2:{}".format(cromo1.getAptidao(), cromo2.getAptidao()))
 
-            C = cromo1 + B * (cromo2 - cromo1)
-            C2= cromo2 + B * (cromo1 - cromo2)
+            C = cromo1.getGene() + B * (cromo2.getGene() - cromo1.getGene())
+            C2= cromo2.getGene() + B * (cromo1.getGene() - cromo2.getGene())
 
-            novovetor_cromossomos[i].gene = C
-            novovetor_cromossomos[i+1].gene = C2
+            if C < -20:
+                C = -20
+            if C > 20:
+                C = 20
+            if C2 < -20:
+                C2 = -20
+            if C2 > 20:
+                C2 = 20
 
-            print("Depois")
-            print("p1:{}|p2:{}".format(novovetor_cromossomos[i].getGene(), novovetor_cromossomos[i+1].getGene()))
+            novovetor_cromossomos[i].setGene(C)
+            novovetor_cromossomos[i+1].setGene(C2)
+
+            print("Depois - p1:{}|p2:{}".format(novovetor_cromossomos[i].getAptidao(), novovetor_cromossomos[i+1].getAptidao()))
             print("####################")
+        else:
+            novovetor_cromossomos[i] = cromossomos[r1]
+            novovetor_cromossomos[i + 1] = cromossomos[r2]
+
 
     return novovetor_cromossomos
 
@@ -131,8 +144,10 @@ def add_elite(vetCromo,elite):
 
 
 def printCromo(vetCromo):
+    print("|---------------START-----------------|")
     for cromo in vetCromo:
         cromo.print()
+    print("|---------------END-------------------|")
 
 
 
@@ -150,31 +165,38 @@ globalcross = 80
 maxGeneration = [10,20]
 interations = 10
 
-
-'''vetor_cromossomos = [Cromossomo() for _ in range(10)]  # criando 10 cromossomos
+print("População Iniciada")
+vetor_cromossomos = [Cromossomo() for _ in range(10)]  # criando 10 cromossomos
 for i in vetor_cromossomos:
     i.genes = i.gera_gene()  # parte 1
     i.calc_aptidao()
 
-    # print(i.genes, i.aptidao)
-    
-print("START GENERATION")
-# reservando o elite antes de mudar td
-elite = elitismo(vetor_cromossomos)
-print("Torneio")
-vetor_cromossomos = torneio(vetor_cromossomos)
-print("CrossOver")
-vetor_cromossomos = crossOver(vetor_cromossomos)
-print("Mutation")
-vetor_cromossomos = mutation(vetor_cromossomos)
-print("Aptidation Times")
-vetor_cromossomos = calc_aptidao(vetor_cromossomos)
-print("Elitismo")
-vetor_cromossomos = add_elite(vetor_cromossomos,elite)
-print("print time")
-printCromo(vetor_cromossomos)
-print("END GENERATION ")
-'''
+    print(i.getGene(), i.getAptidao())
+print("inicialização finalizada")
+
+# g = 0
+# maxg = 1
+# printCromo(vetor_cromossomos)
+# print("START GENERATION")
+# # reservando o elite antes de mudar td
+# elite = elitismo(vetor_cromossomos)
+# printCromo(vetor_cromossomos)
+# print("Torneio")
+# vetor_cromossomos = torneio(vetor_cromossomos)
+# printCromo(vetor_cromossomos)
+# print("CrossOver")
+# vetor_cromossomos = crossOver(vetor_cromossomos)
+# printCromo(vetor_cromossomos)
+# print("Mutation")
+# vetor_cromossomos = mutation(vetor_cromossomos,g,maxg)
+# # print("Aptidation Times")
+# # vetor_cromossomos = calc_aptidao(vetor_cromossomos)
+# printCromo(vetor_cromossomos)
+# print("Elitismo")
+# vetor_cromossomos = add_elite(vetor_cromossomos,elite)
+# printCromo(vetor_cromossomos)
+# print("END GENERATION ")
+
 
 wb = Workbook()
 sheet = wb.add_sheet('algoritmo_genetico_table')
@@ -191,12 +213,14 @@ for generation in maxGeneration:
 
     for j in range(interations):
 
+        print("População Iniciada #########################################################")
         vetor_cromossomos = [Cromossomo() for _ in range(10)]  # criando 10 cromossomos
         for i in vetor_cromossomos:
             i.genes = i.gera_gene()  # parte 1
             i.calc_aptidao()
 
-            print(i.genes, i.aptidao)
+            print(i.getGene(), i.getAptidao())
+        print("inicialização finalizada")
 
         for i in range(generation):
             print("START GENERATION")
@@ -207,18 +231,17 @@ for generation in maxGeneration:
             print("CrossOver")
             vetor_cromossomos = crossOver(vetor_cromossomos)
             print("Mutation")
-            vetor_cromossomos = mutation(vetor_cromossomos,i,generation)
-            print("Aptidation Times")
-            vetor_cromossomos = calc_aptidao(vetor_cromossomos)
+            vetor_cromossomos = mutation(vetor_cromossomos, i, generation)
             print("Elitismo")
-            vetor_cromossomos = add_elite(vetor_cromossomos,elite)
+            vetor_cromossomos = add_elite(vetor_cromossomos, elite)
             print("print time")
             printCromo(vetor_cromossomos)
-            print("END GENERATION ", i)
+            print("END GENERATION ")
 
             eliteSon = elitismo(vetor_cromossomos)
+            if (eliteSon.getAptidao() < -19):
+                exit()
             sheet.write(i+y + 1, x+j+1, eliteSon.getAptidao())
     x = 1
     y = 15
-
 wb.save('algoritmo_genetico_table.xls')
