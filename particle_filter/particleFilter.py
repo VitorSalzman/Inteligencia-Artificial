ef58c01af4a713b0d5b07a42bc734d83920e494a
Luiz Antonio R. G <luizguzzo@gmail.com>
2019-12-06 00:13:26 -0300

    podagem

--- particle_filter/particleFilter.py
+++ particle_filter/particleFilter.py
@@ -1,98 +1,48 @@
-from collections import deque
-from imutils.video import VideoStream
-import numpy as np
-import argparse
-import cv2
 import imutils
-import time
-
-import numpy as np
 import cv2
 
-ap = argparse.ArgumentParser()
-ap.add_argument("-v", "--video",
-	help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64,
-	help="max buffer size")
-args = vars(ap.parse_args())
+# Rastreamento da bola de basquete. Codigo origem: https://www.pyimagesearch.com/2015/09/14/ball-tracking-with-opencv/
 
-cap = cv2.VideoCapture('basket.mp4')
+# Pra cancelar o loop é so segura "c" (uma hora ele para xD)
+
+# Requisito de download:
+# pip install --upgrade imutils
+# pip install opencv-python
+
+cap = cv2.VideoCapture('basket.mp4') # poem o nome do arquivo do video do professor aqui
 
 greenLower = (0, 0, 0)
 greenUpper = (11, 255, 255)
-pts = deque(maxlen=args["buffer"])
 
 while(cap.isOpened()):
 
     ret, frame = cap.read()
     if ret:
-        cv2.imshow("Image", frame)
-
-        # resize the frame, blur it, and convert it to the HSV
-        # color space
-        frame = imutils.resize(frame, width=600)
-        blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+        blurred = cv2.GaussianBlur(frame, (3, 3), 0)
         hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-        cv2.imshow('hsv',hsv)
-
-        # construct a mask for the color "green", then perform
-        # a series of dilations and erosions to remove any small
-        # blobs left in the mask
         mask = cv2.inRange(hsv, greenLower, greenUpper)
         mask = cv2.erode(mask, None, iterations=2)
         mask = cv2.dilate(mask, None, iterations=2)
-
-        # find contours in the mask and initialize the current
-        # (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
-                                cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = imutils.grab_contours(cnts)
         center = None
-
-        # only proceed if at least one contour was found
         if len(cnts) > 0:
-            # find the largest contour in the mask, then use
-            # it to compute the minimum enclosing circle and
-            # centroid
             c = max(cnts, key=cv2.contourArea)
             ((x, y), radius) = cv2.minEnclosingCircle(c)
             M = cv2.moments(c)
             center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-            # only proceed if the radius meets a minimum size
             if radius > 10:
-                # draw the circle and centroid on the frame,
-                # then update the list of tracked points
-                cv2.circle(frame, (int(x), int(y)), int(radius),
-                           (0, 255, 255), 2)
-                cv2.circle(frame, center, 5, (0, 0, 255), -1)
+                cv2.circle(frame, (int(x), int(y)), int(radius),(0, 255, 255), 2) # corpo da bola
+                cv2.circle(frame, center, 5, (0, 0, 255), -1) #centro da detecção
 
-        # update the points queue
-        pts.appendleft(center)
-
-        # loop over the set of tracked points
-        for i in range(1, len(pts)):
-            # if either of the tracked points are None, ignore
-            # them
-            if pts[i - 1] is None or pts[i] is None:
-                continue
-
-            # otherwise, compute the thickness of the line and
-            # draw the connecting lines
-            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
-
-        # show the frame to our screen
         cv2.imshow("Frame", frame)
         key = cv2.waitKey(1) & 0xFF
     else:
-        cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+        cap.set(cv2.CAP_PROP_POS_FRAMES, 0) # motivo do loop eterno
 
 
     if cv2.waitKey(1) & 0xFF == ord('c'):
         break
-
 
 cap.release()
 cv2.destroyAllWindows()
